fd1e093982468bea929769b538bcbd0971f2241a
SAMANYU07 <samanyu1003@gmail.com>
2024-08-02 21:40:17 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://bajaj-backend-roan.vercel.app/",
+      "/api": "https://bajaj-backend-roan.vercel.app",
     }
   }
 })
